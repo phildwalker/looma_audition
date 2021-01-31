21938ffda3b6279713e195aefd98d1d62676755c
Walker <philip.walker@conehealth.com>
2021-01-31 10:30:24 -0500

    update via make

---
+++ src/looma_theme.R
@@ -0,0 +1,39 @@
+looma_theme <- function () { 
+  font <- "Montserrat"
+  #base_size=12, base_family="Avenir"
+  theme_minimal() %+replace% 
+    theme(
+      panel.background  = element_blank(),
+      plot.background = element_rect(fill="gray96", colour=NA), 
+      legend.background = element_rect(fill="transparent", colour=NA),
+      legend.key = element_rect(fill="transparent", colour=NA),
+      
+      #text elements
+      plot.title = element_text(             #title
+        family = font,            #set font family
+        size = 20,                #set font size
+        face = 'bold',            #bold typeface
+        hjust = 0,                #left align
+        vjust = 2),               #raise slightly
+      
+      plot.subtitle = element_text(          #subtitle
+        family = font,            #font family
+        size = 12),               #font size
+      
+      plot.caption = element_text(           #caption
+        family = font,            #font family
+        size = 9,                 #font size
+        hjust = 1),               #right align
+      
+      axis.title = element_text(             #axis titles
+        family = font,            #font family
+        size = 10),               #font size
+      
+      axis.text = element_text(              #axis text
+        family = font,            #axis famuly
+        size = 9),                #font size
+      
+      axis.text.x = element_text(            #margin for axis text
+        margin=margin(5, b = 10))
+    )
+}
